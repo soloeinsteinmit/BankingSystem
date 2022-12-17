ede31773a9055717479f7326e28e6df62e4972a8
soloeinsteinmit <solomoneinsteinmit300@gmail.com>
2022-12-17 00:09:14 +0000

    Created vboxes for the different layout and their fxml and controllers as well

---
+++ src/main/java/com/example/bankingsystem/Login_SignUp_Classes/SignInIBankController.java
@@ -0,0 +1,13 @@
+package com.example.bankingsystem.Login_SignUp_Classes;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignInIBankController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
